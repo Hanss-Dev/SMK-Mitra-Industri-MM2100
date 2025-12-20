76aa75de9fdeaa3b6d83c2e8dc91e5338c36b0a4
Hanss-Dev <naufal2006farhan@gmail.com>
2025-12-20 10:48:28 +0700

    20/12/25

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,41 @@ if (navToggle && navMenu) {
     navToggle.classList.toggle("show-icon");
   });
 };
+
+const slides = document.querySelectorAll('.yuko-item');
+const texts = document.querySelectorAll('.yuko-content-item');
+const bg = document.querySelector('.yuko-sldr-bg');
+const dotsWrap = document.querySelector('.yuko-dots');
+let index = 0;
+
+/* dots */
+slides.forEach((_, i) => {
+  const dot = document.createElement('span');
+  if (i === 0) dot.classList.add('active');
+  dot.onclick = () => go(i);
+  dotsWrap.appendChild(dot);
+});
+const dots = dotsWrap.children;
+
+function go(i) {
+  slides[index].classList.remove('active');
+  texts[index].classList.remove('active');
+  dots[index].classList.remove('active');
+
+  index = i;
+
+  slides[index].classList.add('active');
+  texts[index].classList.add('active');
+  dots[index].classList.add('active');
+
+  bg.style.backgroundImage = `url(${slides[index].dataset.bg})`;
+}
+
+document.getElementById('prev').onclick = () =>
+  go((index - 1 + slides.length) % slides.length);
+
+document.getElementById('next').onclick = () =>
+  go((index + 1) % slides.length);
+
+/* init */
+bg.style.backgroundImage = `url(${slides[0].dataset.bg})`;
